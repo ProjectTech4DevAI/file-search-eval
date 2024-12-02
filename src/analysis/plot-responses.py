f4e934915f24d15c444a3c38912fc2fa00fc43c6
Jerome White <jerome@thewex.net>
2024-12-02 22:51:41 +0530

    Build plots per document set

--- src/analysis/plot-responses.py
+++ src/analysis/plot-responses.py
@@ -15,48 +15,53 @@ if __name__ == '__main__':
     arguments.add_argument('--height-scale', type=float, default=1.5)
     args = arguments.parse_args()
 
-    #
-    #
-    #
-    x = 'score'
+    (x, y) = ('score', 'user')
     components = {
-        'y': 'user',
+        'y': y,
         'hue': 'system',
     }
     xlabel = '' if args.model is None else args.model + ' '
 
-    #
-    #
-    #
-    df = (pd
-          .read_csv(sys.stdin)
-          .groupby(list(components.values()), sort=False)[x]
-          .mean()
-          .reset_index())
+    df = pd.read_csv(sys.stdin)
+    for (d, g) in df.groupby('docs', sort=False):
+        output = (args
+                  .output
+                  .joinpath(d.replace('/', '_'))
+                  .with_suffix('.png'))
+        data = (g
+                .groupby(list(components.values()), sort=False)[x]
+                .mean()
+                .reset_index()
+                .sort_values(by=y))
 
-    #
-    #
-    #
-    fig = plt.gcf()
-    (width, height) = fig.get_size_inches()
-    fig.set_size_inches(width, height * 2)
+        fig = plt.gcf()
+        (width, height) = fig.get_size_inches()
+        fig.set_size_inches(width, height * 2)
 
-    sns.stripplot(
-        x=x,
-        data=df,
-        jitter=True,
-        **components,
-    )
+        sns.stripplot(
+            x=x,
+            data=data,
+            jitter=True,
+            **components,
+        )
 
-    plt.xlabel(f'{xlabel}score')
-    plt.ylabel('User prompt')
-    plt.xlim(args.lowest_score - 1, args.highest_score + 1)
-    plt.grid(visible=True, axis='both', alpha=0.25, linestyle='dotted')
-    plt.legend(
-        title='System prompt',
-        loc='upper center',
-        bbox_to_anchor=(0.2, 1.12),
-        ncol=2,
-    )
+        plt.xlabel(f'{xlabel}score')
+        plt.ylabel('User prompt')
+        plt.xlim(args.lowest_score - 1, args.highest_score + 1)
+        plt.grid(visible=True, axis='both', alpha=0.25, linestyle='dotted')
+        plt.legend(
+            title='System prompt',
+            loc='upper center',
+            bbox_to_anchor=(0.2, 1.12),
+            ncol=2,
+        )
+        plt.title(d)
+        plt.axvline(
+            x=d[x].mean(),
+            color='black',
+            linestyle='dashed',
+            alpha=0.5,
+        )
 
-    plt.savefig(args.output, bbox_inches='tight')
+        plt.savefig(output, bbox_inches='tight')
+        plt.close()
