2073e1a481adfd174195aa0a3d674811d7cd9925
Jerome White <jerome@thewex.net>
2024-12-02 20:27:15 +0530

    Evaluations follow stdin/JSONL semantics

--- bin/run-evals.sh
+++ bin/run-evals.sh
@@ -7,17 +7,13 @@ export PYTHONPATH=$ROOT
 _repetition=3
 _prompts=$ROOT/docs/prompts
 
-while getopts 'i:o:n:g:h' option; do
+while getopts 'n:g:h' option; do
     case $option in
-	i) _input=$OPTARG ;;
-        o) _output=$OPTARG ;;
 	g) _gt=$OPTARG ;;
 	n) _repetition=$OPTARG ;;
         h)
             cat <<EOF
 Usage: $0
- -i Directory containing LLM responses
- -o Directory to deposit experiments and results
  -g Directory containing reference responses
  -n Number of times to repeat each judgement (default $_repetition)
 EOF
@@ -30,29 +26,9 @@ EOF
     esac
 done
 
-#
-#
-#
-e_out=$_output/experiments
-mkdir --parents $e_out
-
 python $ROOT/src/evaluate/build.py \
        --user-prompt $_prompts/evaluate/user \
        --system-prompt $_prompts/evaluate/system \
        --ground-truth $_gt \
        --repetition $_repetition \
-       --predictions $_input \
-       --output $e_out
-
-#
-#
-#
-r_out=$_output/results
-mkdir --parents	$r_out
-
-for i in $e_out/*; do
-    out=$r_out/`basename $i`
-    cat <<EOF
-python $ROOT/src/evaluate/run.py < $i > $out
-EOF
-done | parallel --will-cite --line-buffer --delay 4
+    | python $ROOT/src/evaluate/run.py
