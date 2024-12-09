73855a5b1249660679197d8eab988e9527d03195
Jerome White <jerome@thewex.net>
2024-12-09 20:18:40 +0530

    Combine OpenAI and Deepeval into single pipeline

--- bin/run-evals.sh
+++ bin/run-evals.sh
@@ -5,11 +5,12 @@ ROOT=`git rev-parse --show-toplevel`
 export PYTHONPATH=$ROOT
 
 _repetition=3
-_src=$ROOT/src/evaluate/openai_
+_src=$ROOT/src/evaluate
 
-while getopts 'n:g:h' option; do
+while getopts 'g:p:n:h' option; do
     case $option in
-	g) _gt=$OPTARG ;;
+	g) _gt="--ground-truth $OPTARG" ;;
+	p) _prompts=$OPTARG ;;
 	n) _repetition=$OPTARG ;;
         h)
             cat <<EOF
@@ -26,9 +27,11 @@ EOF
     esac
 done
 
-python $_src/build.py \
-       --user-prompt $_src/user.txt \
-       --system-prompt $_src/system.txt \
-       --ground-truth $_gt \
+python $_src/build.py $_gt \
        --repetition $_repetition \
-    | python $_src/run.py
+    | python $_src/openai_/run.py $_gt \
+	     --user-prompt $_src/openai_/user.txt \
+	     --system-prompt $_src/openai_/system.txt \
+    | python $_src/deepeval_/run.py $_gt \
+	     --user-prompt $_prompts/user \
+	     --deep-config $_src/deepeval_/geval.json
