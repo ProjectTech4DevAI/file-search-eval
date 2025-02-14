3e8a123e29f9642c455c9fd9cfaa7b5a8988c696
Jerome White <jerome@thewex.net>
2025-02-14 09:12:42 +0530

    Simplify dictionary management

--- mylib/_logs.py
+++ mylib/_logs.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(
     format='[ %(asctime)s %(levelname)s %(filename)s ] %(message)s',
     datefmt='%H:%M:%S',
-    level=os.environ.get('PYTHONLOGLEVEL', 'WARNING').upper(),
+    level=os.getenv('PYTHONLOGLEVEL', 'WARNING').upper(),
 )
 logging.captureWarnings(True)
 Logger = logging.getLogger(__name__)
