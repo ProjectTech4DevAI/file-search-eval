a5c1483f65dd3a783d14a9c7702b003f15a38f54
Jerome White <jerome@thewex.net>
2025-04-03 14:02:17 +0530

    Add evaluation methods to different subplots

--- src/analysis/end-to-end/plot-scores.py
+++ src/analysis/end-to-end/plot-scores.py
@@ -1,10 +1,19 @@
 import sys
+import functools as ft
 from pathlib import Path
 from argparse import ArgumentParser
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+@ft.singledispatch
+def gather(ax):
+    yield from ax
+
+def gather(ax: Axes):
+    yield ax
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
@@ -12,22 +21,26 @@ if __name__ == '__main__':
     args = arguments.parse_args()
 
     df = pd.read_csv(sys.stdin)
-    assert df['method'].nunique() == 1
+    methods = df.groupby('method', sort=False)
 
-    ax = sns.barplot(
-        x='score',
-        y='system',
-        hue='docs',
-        data=df,
-    )
+    (fig, axes) = plt.subplots(nrows=methods.ngroups, sharex=True)
 
-    ax.set_xlabel('Score ({})'.format(df['method'].unique().item()))
-    ax.set_ylabel('System prompt')
-    ax.set_xlim(0, 1)
-    ax.grid(visible=True, axis='x', alpha=0.5, linestyle='dotted')
+    for (ax, (m, g)) in zip(gather(axes), methods):
+        sns.barplot(
+            x='score',
+            y='system',
+            hue='docs',
+            data=g,
+            ax=ax,
+        )
 
-    ax.legend().remove()
-    fig = ax.get_figure()
-    fig.legend(loc='outside upper center', fontsize='x-small')
+        ax.set_xlabel('Score ({})'.format(m))
+        ax.set_ylabel('System prompt')
+        ax.set_xlim(0, 1)
+        ax.grid(visible=True, axis='x', alpha=0.5, linestyle='dotted')
+
+        ax.legend().remove()
+        fig = ax.get_figure()
+        fig.legend(loc='outside upper center', fontsize='x-small')
 
     plt.savefig(args.output, bbox_inches='tight')
